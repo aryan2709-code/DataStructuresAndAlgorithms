d8bfb96fcad6179dd4781aa7e49283f06135d801
Aryan <bhardwaj7a@gmail.com>
2025-07-15 12:39:03 +0530

    Detecting a Cycle In Directed Graph Using DFS(Vis array and PathVis array)

---
+++ cycleDetectionUsingDFSinDirectedGraph.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool dfsCheck(int node, vector<int> adj[] , int vis[] , int pathVis[])
+{
+    vis[node] = 1;
+    pathVis[node] = 1;
+
+    for(int neighbour : adj[node])
+    {
+        if(!vis[neighbour])
+        {
+            if(dfsCheck(neighbour,adj,vis,pathVis))
+            return true;
+        }
+        else if(pathVis[neighbour])
+        return true;
+    }
+    // If none of the neighbours return a true , return false
+    // But before that mark the current node as 0 in pathVis array , because it has been visited and will no longer exist on this path
+    pathVis[node] = 0;
+    return false;
+}
+
+bool isCycle(int v,vector<int> adj[])
+{
+    int vis[v] = {0};
+    int pathVis[v] = {0};
+    for(int i=0; i<v; i++)
+    {
+        if(!vis[i])
+        {
+            if(dfsCheck(i,adj,vis,pathVis))
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+vector<int> adjWithCycle[5];
+
+adjWithCycle[0] = {1};
+adjWithCycle[1] = {2};
+adjWithCycle[2] = {3};
+adjWithCycle[3] = {1}; // Cycle here: 1 → 2 → 3 → 1
+adjWithCycle[4] = {};
+
+vector<int> adjNoCycle[6];
+adjNoCycle[0] = {1, 2};
+adjNoCycle[1] = {3};
+adjNoCycle[2] = {3, 4};
+adjNoCycle[3] = {5};
+adjNoCycle[4] = {5};
+adjNoCycle[5] = {};
+
+cout<< boolalpha;
+cout << isCycle(5,adjWithCycle)<<" ";
+cout << isCycle(6,adjNoCycle);
+return 0;
+}
